eb3d7c579090271c835e2e932cd8229dbf0636fb
MykhailDovmantovych <m.dovmantovych@andersenlab.com>
2018-11-24 00:08:12 +0200

    ##### package.json - added drag-resize library
    ##### VideoDropdown.vue
    - added styles and functionality for draggable
    
    ##### Home.vue
    - added styles and functionality for draggable video windows

--- src/components/VideoDropdown.vue
+++ src/components/VideoDropdown.vue
@@ -1,8 +1,9 @@
 <!-- eslint-disable -->
 <template>
   <div class="">
-    <div>
-        <iframe width="560" height="315" v-bind:src="'https://www.youtube.com/embed/'+ videoSource" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div class="iframe-description" >Click here to start dragging</div>
+    <div class="iframe-container">
+        <iframe  v-bind:src="'https://www.youtube.com/embed/'+ videoSource" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
   </div>
 </template>
@@ -14,12 +15,41 @@ export default Vue.extend({
     name: 'VideoDropdown',
     props:{
        videoSource:String
+    },
+    methods:{
     }
 })
 </script>
 
 <style lang="scss" scoped>
-iframe{
-    width:30%;
+.iframe-container {
+  overflow: hidden;
+  padding-top: 56.25%;
+  position: relative;
+}
+
+.iframe-container iframe {
+    border-color: rgb(241, 229, 224);
+   border-top-width: 20px;
+   border-top-style:double;
+   border-top-left-radius: 20%;
+   height: 90%;
+   left: 0;
+   position: absolute;
+   top: 0;
+   width: 100%;
+}
+.iframe-description{
+ position:absolute;
+ margin:0 auto;
+ width: 95%;
+ z-index: 99;
+ font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+ font-size: 20px;
+ cursor: pointer;
+}
+/* 4x3 Aspect Ratio */
+.iframe-container-4x3 {
+  padding-top: 75%;
 }
 </style>
